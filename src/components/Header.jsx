e6b02da87dfc8c56bef630c169764467be781124
FrancescoCaporale <frenk.caporale@gmail.com>
2026-01-19 14:30:55 +0100

    Aggiunto main

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>DC comics</h1>
+            <nav>
+                <ul>
+                    <li>Characters</li>
+                    <li>Comics</li>
+                    <li>Movies</li>
+                    <li>Games</li>
+                    <li>Collectibles</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
---
+++ src/components/main.jsx
@@ -0,0 +1,7 @@
+export function main() {
+  return (
+    <main>
+      <h1>{`--> Content Goes here <--`}</h1>
+    </main>
+  )
+}
